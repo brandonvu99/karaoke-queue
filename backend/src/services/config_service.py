69c8a0e7bf77fb0f4e1412dcc05648fe929ad814
Brandon Vu <bvu32@gatech.edu>
2021-05-08 19:17:10 -0400

    commented out command line arg parsing for config file

--- backend/src/services/config_service.py
+++ backend/src/services/config_service.py
@@ -2,13 +2,14 @@ import yaml
 import os
 import argparse
 
-parser = argparse.ArgumentParser(description='Starts up a flask app to host an API on this machine.')
-parser.add_argument('--configuration', help="Name of configuration file. Should be in the \"configs/\" directory.", required=True)
-args = parser.parse_args()
+# parser = argparse.ArgumentParser(description='Starts up a flask app to host an API on this machine.')
+# parser.add_argument('--configuration', help="Name of configuration file. Should be in the \"configs/\" directory.", required=True)
+# args = parser.parse_args()
 
-path_to_config = f"configs/{args.configuration}"
-if not os.path.exists(path_to_config):
-    raise ValueError(f"Given --configuration value is invalid because the path {path_to_config}\" does not exist.")
+# path_to_config = f"configs/{args.configuration}"
+# if not os.path.exists(path_to_config):
+#     raise ValueError(f"Given --configuration value is invalid because the path {path_to_config}\" does not exist.")
 
+path_to_config = f"configs/dev_local.yaml"
 with open(path_to_config) as f:
     configuration = yaml.load(f, Loader=yaml.FullLoader)
