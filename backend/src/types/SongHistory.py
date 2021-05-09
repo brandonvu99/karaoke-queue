3d461282ec5ffba9a7e6b20cb29943ab7634e303
Brandon Vu <bvu32@gatech.edu>
2021-05-09 18:12:41 -0400

    fixed duplicate songs showing up with SongHistory class

--- backend/src/types/SongHistory.py
+++ backend/src/types/SongHistory.py
@@ -1,26 +1,33 @@
-from src.types.Song import BaseSong, setup_model
+from src.types.Song import BaseSong
 
 from src.services import config_service
 
-try:
-    dynamodb_host = config_service.configuration['aws']['dynamodb']['host-url']
-except KeyError:
-    dynamodb_host = None
+class SongHistory(BaseSong):
+    class Meta:
+        table_name = "songs-history"
+        aws_access_key_id = "***REMOVED***"
+        aws_secret_access_key = "***REMOVED***"
 
-try:
-    aws_access_key_id = config_service.configuration['aws']['access-key-id']
-except KeyError:
-    aws_access_key_id = None
 
-try:
-    aws_secret_access_key = config_service.configuration['aws']['secret-access-key']
-except KeyError:
-    aws_secret_access_key = None
+# try:
+#     dynamodb_host = config_service.configuration['aws']['dynamodb']['host-url']
+# except KeyError:
+#     dynamodb_host = None
 
-SongHistory = setup_model(
-    BaseSong,
-    "songs-history",
-    dynamodb_host,
-    aws_access_key_id,
-    aws_secret_access_key
-)
+# try:
+#     aws_access_key_id = config_service.configuration['aws']['access-key-id']
+# except KeyError:
+#     aws_access_key_id = None
+
+# try:
+#     aws_secret_access_key = config_service.configuration['aws']['secret-access-key']
+# except KeyError:
+#     aws_secret_access_key = None
+
+# SongHistory = setup_model(
+#     BaseSong,
+#     "songs-history",
+#     dynamodb_host,
+#     aws_access_key_id,
+#     aws_secret_access_key
+# )
