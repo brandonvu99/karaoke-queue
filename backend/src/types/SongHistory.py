ba24af0d38d8d2df2deb72f7c38703244ab80021
Brandon Vu <bvu32@gatech.edu>
2021-05-09 19:34:16 -0400

    fixed duplicate songs being saved (had to create completely separate model classes)

--- backend/src/types/SongHistory.py
+++ backend/src/types/SongHistory.py
@@ -1,33 +1,75 @@
-from src.types.Song import BaseSong
+from pynamodb.attributes import MapAttribute, NumberAttribute, UnicodeAttribute, UnicodeSetAttribute, UTCDateTimeAttribute
+from pynamodb.models import Model
+from pynamodb.settings import default_settings_dict
 
 from src.services import config_service
+from src.types.utils import setup_model
 
-class SongHistory(BaseSong):
+class SongHistoryWithoutConfiguration(Model):
     class Meta:
-        table_name = "songs-history"
-        aws_access_key_id = "AKIATNJBQMGJEKFIOLU3"
-        aws_secret_access_key = "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"
+        write_capacity_units = 5
+        read_capacity_units = 5
 
+    # Partition Key, tells us which Song_Queue this Song belongs to
+    song_queue_id = UnicodeAttribute(hash_key=True)
 
-# try:
-#     dynamodb_host = config_service.configuration['aws']['dynamodb']['host-url']
-# except KeyError:
-#     dynamodb_host = None
+    # Sort Key, id that uniquely identifies a Song
+    id = UnicodeAttribute(range_key=True)
 
-# try:
-#     aws_access_key_id = config_service.configuration['aws']['access-key-id']
-# except KeyError:
-#     aws_access_key_id = None
+    # ID of the requestor of the Song
+    requester_id = UnicodeAttribute()
 
-# try:
-#     aws_secret_access_key = config_service.configuration['aws']['secret-access-key']
-# except KeyError:
-#     aws_secret_access_key = None
+    # Time of Song request in epoch milliseconds
+    date_created = UTCDateTimeAttribute()
 
-# SongHistory = setup_model(
-#     BaseSong,
-#     "songs-history",
-#     dynamodb_host,
-#     aws_access_key_id,
-#     aws_secret_access_key
-# )
+    # Song Artist name
+    artist = UnicodeAttribute()
+
+    # Song name
+    song_name = UnicodeAttribute()
+
+    # Song duration
+    duration_ms = NumberAttribute(default=0) # UnicodeAttribute(default="03:15")
+
+    # A set of requester_id's that have upvoted this song
+    upvotes = UnicodeSetAttribute(default=dict)
+
+    # Song image url
+    image_url = UnicodeAttribute()
+
+    def __eq__(self, o: object) -> bool:
+        if self is o:
+            return True
+        return ((o is not None) and \
+                (o.__class__ == SongHistoryWithoutConfiguration) and \
+                (o.id == self.id))
+    
+    def __iter__(self):
+        for name, attr in self.get_attributes().items():
+            if isinstance(attr, MapAttribute):
+                yield name, getattr(self, name).as_dict()
+            else:
+                yield name, attr.serialize(getattr(self, name))
+
+try:
+    dynamodb_host = config_service.configuration['aws']['dynamodb']['host-url']
+except KeyError:
+    dynamodb_host = None
+
+try:
+    aws_access_key_id = config_service.configuration['aws']['access-key-id']
+except KeyError:
+    aws_access_key_id = None
+
+try:
+    aws_secret_access_key = config_service.configuration['aws']['secret-access-key']
+except KeyError:
+    aws_secret_access_key = None
+
+SongHistory = setup_model(
+    SongHistoryWithoutConfiguration,
+    "songs-history",
+    dynamodb_host,
+    aws_access_key_id,
+    aws_secret_access_key
+)
