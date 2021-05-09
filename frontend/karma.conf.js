9c08a0047c1ab8e753dfe13b798fbd41b22cbe3e
Brandon Vu <bvu32@gatech.edu>
2021-05-08 21:03:36 -0400

    changed app name in front-end to be karaoke-queue-app

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/karaoke'),
+      dir: require('path').join(__dirname, './coverage/karaoke-queue-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'karaoke'`, () => {
+  it(`should have as title 'karaoke-queue-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('karaoke');
+    expect(app.title).toEqual('karaoke-queue-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('karaoke app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('karaoke-queue-app app is running!');
   });
 });
